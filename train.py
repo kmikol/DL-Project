535e41d74497afb367ac35a0408c28d677eccd2e
kmikol <41921516+kmikol@users.noreply.github.com>
2021-11-16 08:45:55 +0100

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Nov  2 10:33:12 2021
 
-@author: Kamil
+@author: 
 """
 
 import torch
@@ -25,7 +25,7 @@ from data_loader import Loader, splitData
 img_path = "C:\\Users\\Kamil\\OneDrive - Danmarks Tekniske Universitet\\DTU_courses\\Deep_Learning\\project\\images\\"
 
 data = Loader(img_size=[224,224],img_path=img_path)
-train_set,test_set,val_set = splitData(data,0.6,0.2,0.2)
+train_set,test_set,val_set = splitData(data,0.6,0.2,0.2,seed=0)
 print(f'Number of images:\nTrain: {len(train_set)}\nTest: {len(test_set)}\nVal: {len(val_set)}')
 
 #%% Check if all classes are represented in train, test and val set
@@ -56,16 +56,16 @@ for i in range(len(diagnosis_train)):
 diagnosis_frac = diagnosis_count/diagnosis_count.sum()
 diagnosis_weight = (1-diagnosis_frac)/(1-diagnosis_frac).sum()
 
-#%% Compute feature weights for unbalanced data
+#%% Compute characteristic weights for unbalanced data
 i=0
-feature_count = torch.zeros(7)
+characteristic_count = torch.zeros(7)
 for key in train_set.data.keys()[3:-1]:
-    feature_count[i] = (train_set.data[key]>0).sum()
+    characteristic_count[i] = (train_set.data[key]>0).sum()
     i+=1
 
-feature_weight = (1-feature_count/len(train_set)).to(device)
+characteristic_weight = (1-characteristic_count/len(train_set)).to(device)
 
-feature_names = train_set.data.keys()[3:-1].tolist()
+characteristic_names = train_set.data.keys()[3:-1].tolist()
 
 #%% Set train set augmentation
 
@@ -87,56 +87,63 @@ train_set.setTransform(train_transform)
 def t2i(img):
     return torch.swapaxes(torch.swapaxes(img,1,2),0,2)
 
-img,diagnosis,feature = val_set[0]
+img,diagnosis,characteristic = train_set[0]
 plt.imshow(t2i(img))
 plt.show()
 
 #%% Model setup
 
 class Model(torch.nn.Module):
-    def __init__(self,num_diagnosis,num_features):
+    def __init__(self,num_diagnosis,num_characteristics,base_model = 'resnet'):
         super(Model,self).__init__()
         
-        model = models.resnet18(pretrained=True)
-        model.fc = torch.nn.Linear(model.fc.in_features, num_diagnosis+num_features)
+        if base_model == 'densenet':
+            model = models.densenet121(pretrained=True)
+            model.classifier = torch.nn.Linear(model.classifier.in_features, num_diagnosis+num_characteristics)
+        
+        elif base_model == 'resnet':
+            model = models.resnet18(pretrained=True)
+            model.fc = torch.nn.Linear(model.fc.in_features, num_diagnosis+num_characteristics)
+        else:
+            raise Exception('base model not available')
         
         for param in model.parameters():
             param.requires_grad=True
         
         self.model = model
         self.num_diagnosis = num_diagnosis
-        self.num_features = num_features
+        self.num_characteristics = num_characteristics
         
     def forward(self,x):
         
         y = self.model(x)
         diagnosis_enc = y[:,:self.num_diagnosis]
-        feature_enc = y[:,self.num_diagnosis:]
+        characteristic_enc = y[:,self.num_diagnosis:]
         
-        return diagnosis_enc, feature_enc
+        return diagnosis_enc, characteristic_enc
   
-def bce(y_hat,y,weights = feature_weight ):
+def bce(y_hat,y,weights = characteristic_weight ):
     p = torch.nn.Sigmoid()(torch.clip(y_hat,-10,10))
-    loss = -torch.mean(feature_weight*y*torch.log(p) + (1-feature_weight)*(1-y)*torch.log(1-p))
+    loss = -torch.mean(characteristic_weight*y*torch.log(p) + (1-characteristic_weight)*(1-y)*torch.log(1-p))
     return loss
       
 def lossFunction(y_hat,y):
     
     y_hat_diagnosis = y_hat[0]
-    y_hat_feature = y_hat[1]
+    y_hat_characteristic = y_hat[1]
     y_diagnosis = y[0]
-    y_feature = y[1]
+    y_characteristic = y[1]
         
     
     xEntropy = torch.nn.CrossEntropyLoss(weight=diagnosis_weight.to(device),reduce='mean')
     #bce = torch.nn.BCELoss(reduce='mean')
     sigmoid = torch.nn.Sigmoid()
     xEntropy_loss = xEntropy(y_hat_diagnosis,y_diagnosis)
-    bce_loss = bce(y_hat_feature,y_feature)
+    bce_loss = bce(y_hat_characteristic,y_characteristic)
     
     if bce_loss<0:
-        print(sigmoid(y_hat_feature))
-        print(y_feature)
+        print(sigmoid(y_hat_characteristic))
+        print(y_characteristic)
         print(bce_loss)
     
     return xEntropy_loss, bce_loss
@@ -154,11 +161,11 @@ def train(model,train_set,test_set,lossFunction,optimizer,num_epochs=10,batch_si
     
     history = {'train_loss':[],
                'train_diagnosis_loss':[],
-               'train_feature_loss':[],
+               'train_characteristic_loss':[],
                'train_accuracy':[],
                'test_loss':[],
                'test_diagnosis_loss':[],
-               'test_feature_loss':[],
+               'test_characteristic_loss':[],
                'test_accuracy':[]}
     
     N_train = len(train_set)
@@ -179,18 +186,18 @@ def train(model,train_set,test_set,lossFunction,optimizer,num_epochs=10,batch_si
                 
         train_loss = 0
         train_diagnosis_loss = 0
-        train_feature_loss = 0
+        train_characteristic_loss = 0
         train_correct = 0
         
         test_loss = 0
         test_diagnosis_loss = 0
-        test_feature_loss = 0
+        test_characteristic_loss = 0
         test_correct = 0
         
         model.train()
-        for img,disease,feature in tqdm(train_iterator, desc='train: '):
+        for img,disease,characteristic in tqdm(train_iterator, desc='train: '):
             X = img.to(device).float()
-            y = [disease.to(device), feature.to(device)]
+            y = [disease.to(device), characteristic.to(device)]
             
             curr_batch_size = disease.shape[0]
             
@@ -203,17 +210,17 @@ def train(model,train_set,test_set,lossFunction,optimizer,num_epochs=10,batch_si
             
             train_loss += curr_batch_size * loss / N_train
             train_diagnosis_loss += curr_batch_size * xEntropy_loss / N_train
-            train_feature_loss += curr_batch_size * bce_loss / N_train
+            train_characteristic_loss += curr_batch_size * bce_loss / N_train
             
             predicted = y_hat[0].argmax(1)
             train_correct += (y[0]==predicted).sum().item()
         
         model.eval()
-        for img,disease,feature in tqdm(test_iterator, desc='test: '):
+        for img,disease,characteristic in tqdm(test_iterator, desc='test: '):
             
             with torch.no_grad():
                 X = img.to(device).float()
-                y = [disease.to(device), feature.to(device)]
+                y = [disease.to(device), characteristic.to(device)]
                 
                 curr_batch_size = disease.shape[0]
             
@@ -223,18 +230,18 @@ def train(model,train_set,test_set,lossFunction,optimizer,num_epochs=10,batch_si
             
                 test_loss += curr_batch_size * loss / N_test
                 test_diagnosis_loss += curr_batch_size * xEntropy_loss / N_test
-                test_feature_loss += curr_batch_size * bce_loss / N_test
+                test_characteristic_loss += curr_batch_size * bce_loss / N_test
                 
                 predicted = y_hat[0].argmax(1)
                 test_correct += (y[0]==predicted).sum().item()
             
         history['train_loss'].append(train_loss.item())
         history['train_diagnosis_loss'].append(train_diagnosis_loss.item())
-        history['train_feature_loss'].append(train_feature_loss.item())
+        history['train_characteristic_loss'].append(train_characteristic_loss.item())
         history['train_accuracy'].append(train_correct/N_train)
         history['test_loss'].append(test_loss.item())
         history['test_diagnosis_loss'].append(test_diagnosis_loss.item())
-        history['test_feature_loss'].append(test_feature_loss.item())
+        history['test_characteristic_loss'].append(test_characteristic_loss.item())
         history['test_accuracy'].append(test_correct/N_test)
         
 
@@ -246,14 +253,18 @@ def train(model,train_set,test_set,lossFunction,optimizer,num_epochs=10,batch_si
         
     return history
 
-
-
+#%% Train the model
 history = train(model,train_set,test_set,lossFunction,optimizer,
-                num_epochs=100,batch_size=8,num_workers=0,save_path='resnet18_1')
+                    num_epochs=500,batch_size=16,num_workers=0,save_path='resnet18_3')
+
+#%% Load pretrained model for evaluation
+model = torch.load('resnet18_2',map_location='cpu')
+model.eval()
+model.to(device)
 
 
 #%% Plot history
-
+# Works only if the model was trained during the same session (history is not saved)
 e = np.arange(1,len(history['train_loss'])+1)
 plt.plot(e,history['train_loss'],label='train')
 plt.plot(e,history['test_loss'],label='test')
@@ -261,19 +272,18 @@ plt.title('Loss')
 plt.xlabel('Epoch')
 plt.legend(); plt.grid(); plt.show()
 
-
 plt.plot(e,history['train_accuracy'],label='train')
 plt.plot(e,history['test_accuracy'],label='test')
 plt.title('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(); plt.grid(); plt.show()
 
-#%%
+#%% Show the contribution of diagnosis and characteristics loss
 e = np.arange(1,len(history['train_loss'])+1)
 plt.plot(e,history['train_diagnosis_loss'],label='train diagnosis')
-plt.plot(e,history['train_feature_loss'],label='train feature')
+plt.plot(e,history['train_characteristic_loss'],label='train characteristic')
 plt.plot(e,history['test_diagnosis_loss'],label='test diagnosis')
-plt.plot(e,history['test_feature_loss'],label='test feature')
+plt.plot(e,history['test_characteristic_loss'],label='test characteristic')
 plt.title('Loss')
 plt.xlabel('Epoch')
 plt.legend(); plt.grid(); plt.show()
@@ -281,7 +291,7 @@ plt.legend(); plt.grid(); plt.show()
 
 
 
-#%%
+#%% Plot pictures with predictions
 def t2i(I):
     return torch.swapaxes(torch.swapaxes(I,1,2),0,2)
 
@@ -299,21 +309,55 @@ for i in range(5):
     plt.show()
     
 
-#%%
-correct=0
-N = 0
+#%% Evaluate the network
+N = len(val_set)
+diagnosis_pred = np.zeros(N,dtype=int)
+char_pred = np.zeros((N,7))
+
+diagnosis_true = np.zeros_like(diagnosis_pred)
+char_true = np.zeros_like(char_pred)
+
 for i in range(len(val_set)):
-    i,y,f=val_set[i]
-    y_hat = model(i.view(1,3,224,224).to(device))
-    y_hat = y_hat[0].argmax().cpu().numpy()
+    img,y,c=val_set[i]
+    y_hat = model(img.view(1,3,224,224).to(device))
+    y_hat_d = y_hat[0].argmax().cpu().numpy().item()
     y = y.numpy()
     
-    N+=1
-    if y==y_hat:
-        correct+=1
+    diagnosis_pred[i] = y_hat_d
     
+    cp = torch.nn.Sigmoid()(y_hat[1]).cpu().detach().numpy()
+    char_pred[i] =  cp
+    
+    diagnosis_true[i] = y
+    char_true[i] = c.cpu().numpy()
 
-print(correct/N)
+char_pred_b = char_pred>0.5
+    
+#%%
+
+# Plot confusion matrix for the diagnosis
+import seaborn as sb
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+conf_mat = confusion_matrix(diagnosis_true,diagnosis_pred)
+
+df_cm = pd.DataFrame(conf_mat,index=[i for i in diagnosis_val],
+                     columns=[i for i in diagnosis_val])
+
+plt.figure(figsize=(5,5),dpi=300)
+sb.heatmap(df_cm,annot=True,cmap='Blues',cbar=False)
+plt.ylabel('True')
+plt.xlabel('Predicted')
+plt.show()
+
+# Plot accuracy for the characteristics
+char_acc = (char_true==char_pred_b).sum(0)/N
+plt.bar(characteristic_names,char_acc)
+plt.xticks(rotation=90)
+plt.show()
+
+
 
 #%%
 
@@ -357,7 +401,7 @@ def saliencyMap(model,optimizer,img,label,num_imgs,std=0.05,mean=0.0):
     
     
 # This doesnt work properly
-def saliencyMapAll(model,optimizer,img,label,features,num_imgs,std=0.05,mean=0.0):
+def saliencyMapAll(model,optimizer,img,label,characteristics,num_imgs,std=0.05,mean=0.0):
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
@@ -409,31 +453,31 @@ def saliencyMapAll(model,optimizer,img,label,features,num_imgs,std=0.05,mean=0.0
         y_hat_d = y_hat[0]
         y_hat_f = y_hat[1]
         
-        features_true = torch.ones_like(y_hat_f)*features.to(device).detach()
-        features_predicted = torch.ones_like(y_hat_f)*10*(2*features-1).to(device).detach()
-        features_predicted[:,i]=y_hat_f[:,i]
+        characteristics_true = torch.ones_like(y_hat_f)*characteristics.to(device).detach()
+        characteristics_predicted = torch.ones_like(y_hat_f)*10*(2*characteristics-1).to(device).detach()
+        characteristics_predicted[:,i]=y_hat_f[:,i]
         
         
-        loss = bce(features_predicted,features_true)
+        loss = bce(characteristics_predicted,characteristics_true)
         loss.backward()
         
         saliency_map,_ = torch.max(imgs.grad.data.abs(),dim=1)
         saliency_map = torch.mean(saliency_map,dim=0)
         
-        quant = torch.quantile(saliency_map,torch.tensor([0.02,0.98],device=device))
+        quant = torch.quantile(saliency_map,torch.tensor([0.05,0.95],device=device))
         saliency_map = torch.clip(saliency_map,min=quant[0],max=quant[1])
         
         
         plt.subplot(3,3,3+i)
         plt.imshow(saliency_map.detach().cpu().numpy(),cmap='jet')
-        plt.title(feature_names[i])
+        plt.title(characteristic_names[i])
         plt.grid();
     
     plt.show()
 
 for i in range(10):
     img,d,f = val_set[i]
-    saliencyMap(model,optimizer,img,d,50,std=0.1)
+    saliencyMap(model,optimizer,img,d,50,std=0.05)
     
     #saliencyMapAll(model,optimizer,img,d,f,50,std=0.1)
     
